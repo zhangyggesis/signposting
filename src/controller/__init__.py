8cad0a0b094ddfe8ab7d230c71e3e8509de00370
zhangyg <yudong.zhang@gesis.org>
2024-05-08 14:44:38 +0200

    enable to do check with html and http, ehance the json output.

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -1,42 +1,89 @@
 from flask import Flask, request, render_template
 import signposting
-from signposting import cli
-from io import StringIO
-import urllib.request,urllib.parse,urllib.error
-
+import urllib.request, urllib.parse, urllib.error
+from urllib.error import HTTPError, URLError
+from signposting.htmllinks import UnrecognizedContentType, find_signposting_html
+from signposting.linkset import LinksetParseError, find_signposting_linkset
 
 app = Flask(__name__)
+
 
 @app.route("/signposting/check")
 def home():
     landingpage = request.args.get('url')
     response = {}
+    print(str(landingpage))
+    s_html = None
+    s_http = None
+    # ignore all warnings.
+    # warnings.simplefilter('ignore')
 
     try:
-        s = signposting.find_signposting_http(
-            landingpage)
-    except (ValueError, urllib.error.HTTPError) as ve:
+        s_http = signposting.find_signposting_http(landingpage)
+        print("s http " + str(s_http))
+        # linkset is not supported at this moment.
+        # s_linkset = find_signposting_linkset(landingpage)
+        # print("s linkset " + str(s_linkset))
+        print("Signpopsting: HTTP links " + str(s_http))
+
+    except (ValueError, urllib.error.HTTPError, URLError, IOError) as ve:
         response["Error"] = str(ve)
         return response
 
-    else:
-        #print it out
-        print(str(s))
+    except UnrecognizedContentType as uc:
+        pass
 
-        response = {}
-        c = 1
-        response[c] = "Signposting Landing Page: <" + landingpage + ">"
+    try:
+        s_html = signposting.find_signposting_html(landingpage)
+        print("s html " + str(s_html))
+        # linkset is not supported at this moment.
+        # s_linkset = find_signposting_linkset(landingpage)
+        # print("s linkset " + str(s_linkset))
+        print("Signpopsting: HTML links " + str(s_html))
 
+    except (ValueError, urllib.error.HTTPError, URLError, IOError) as ve:
+        response["Error"] = str(ve)
+        return response
+
+    except UnrecognizedContentType as uc:
+        pass
+
+    hm = "htmlLinks"
+    hp = "httpLinks"
+    url = "PID"
+    # The assessment result.
+
+    assessment = {}
+    signposting_html = {}
+    signposting_http = {}
+    response["assessment"] = assessment
+    assessment[hm] = signposting_html
+    assessment[url] = landingpage
+    assessment[hp] = signposting_http
+
+    # populate signposting html and http, starting from html
+    s = s_html
+    if s:
+        c = 0
         for sp in s:
             l = sp.rel + ": <" + sp.target + ">"
-            c = c+1
-            response[c] = l;
+            c = c + 1
+            signposting_html[c] = l;
+    s = s_http
+    if s:
+        c = 0
+        for sp in s:
+            l = sp.rel + ": <" + sp.target + ">"
+            c = c + 1
+            signposting_http[c] = l;
 
-        return  response
+    return response
+
 
 @app.route("/")
 def sitehome():
     return render_template("signposting.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
